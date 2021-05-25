690e6da1f02c905c7ca01844aa5542416c793bbb
unknown <saki.maruyama@pasnet.local>
2021-05-25 14:29:05 +0900

    using php to render html content

--- modules/partials/header.php
+++ modules/partials/header.php
@@ -1,11 +1,22 @@
+<?php
+// Define an array to contain page titles
+$pageTitles = array(
+    'home' => "Home",
+    "profile" => "My Profile",
+);
+// Get page title depend on what is using module. 
+$pageTitle = $pageTitles[$module];
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
   <head>
+    <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet", href="./assets/css/index.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <meta charset="utf-8">
-    <title>My web page</title>
+    <title><?php echo $pageTitle; ?></title>
     <script src="./assets/js/index.js"></script>
 </head>
 
