20f1b0485fb306dd0f9b0e3cde4630a7c4ef645d
unknown <saki.maruyama@pasnet.local>
2021-05-25 13:57:03 +0900

    add my profile

--- index.php
+++ index.php
@@ -1,82 +1,15 @@
-<!DOCTYPE html>
 <?php
+// Get modules need to use for main content by using $_GET
+$module = $_GET['m'];
+
+// If there is no get param to load page, set $module default to home page
+if ($module == null) {
+    $module = 'home';
+}
+
 # Include header
 require __DIR__ . '/modules/partials/header.php';
 # Include main contain
-require __DIR__ . '/modules/home.php';
+require __DIR__ . "/modules/$module.php";
 # Include footer
 require __DIR__ . '/modules/partials/footer.php';
-
-/*
-<html lang="en">
-  <head>
-    <link rel="stylesheet", href="./assets/css/index.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <meta charset="utf-8">
-    <title>My web page</title>
-    <script src="./assets/js/index.js"></script>
-</head>
-
-<body>
-<header>
-    <div>The logo</div>
-    <div id="slogan" class="slogan">The header slogan</div> 
-    <div id="form">
-        <ul>
-            <li>Hi <span>Guest</span></li>
-            <li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li>
-        </ul>        
-        <form id="login">
-            <input type="text" name="username" placeholder="User name" />
-            <input type="password" name="password" placeholder="Password"/>
-            <label><input type="checkbox" name="rememberUsername" />Remember user name </label>
-            <button type="submit" name="Login">Login</button>
-        </form>
-        <form method="GET" id="search">
-            <input type="text" name="keyword" />
-            <i class="material-icons">search</i>
-        </form>
-    </div>
-</header>
-<nav>
-    <ul>
-        <li>Menu 1</li>
-        <li>Menu 2</li>
-        <li>Menu 3</li>
-        <li>Menu 4</li>
-    </ul>
-</nav>
-
-<div id="main">
-    <div id="main-content">
-        <h3> The main content go here</h3>
-    </div>
-    <div id="sidebar">
-        <h3> Sidebar </h3>
-        <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-            <li>Feature 4</li>
-            <li>Feature 5</li>
-            <li>Feature 6</li>
-        </ul>
-    </div>
-</div>
-
-<footer>
-    <ol>
-        <li>Extra info 1</li>
-        <li>Extra info 1</li>
-    </ol>
-    <h3> The footer</h3>
-    <div id="scroll">
-        <a href="#"> <i class="material-icons">arrow_drop_up</i>
-            <i class="material-icons">arrow_drop_up</i>
-        </a>
-    </div>
-</footer>
-</body>
-</html>
-*/
-
--- modules/partials/header.php
+++ modules/partials/header.php
@@ -33,9 +33,7 @@
 </header>
 <nav>
     <ul>
-        <li>Menu 1</li>
-        <li>Menu 2</li>
-        <li>Menu 3</li>
-        <li>Menu 4</li>
+        <li><a href="./index.php?m=home">Home</a></li>
+        <li><a href="./index.php?m=profile">My Profile</a></li>
     </ul>
 </nav>
